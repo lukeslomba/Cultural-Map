aa2f1e42e2f07d9df4320b967209346caa8a8616
lukeslomba <78974344+lukeslomba@users.noreply.github.com>
2021-07-13 18:30:42 -0400

    new locations added 7/13

--- map_creator.py
+++ map_creator.py
@@ -17,29 +17,85 @@ geolocator = Nominatim(user_agent="Luke's mapping app", timeout=2)
 
 lat_list = []
 long_list = []
+add_list = []
 
 for address in response_df['Location']:
     print(address)
     geo = geolocator.geocode(address)
-    print(geo)
-    lat = geo.latitude
-    long = geo.longitude
-    lat_list.append(lat)
-    long_list.append(long)
+    if geo != None:
+        print(geo)
+        lat = geo.latitude
+        long = geo.longitude
+        lat_list.append(lat)
+        long_list.append(long)
+        add_list.append(geo)
+    else:
+        lat_list.append('41.2705')
+        long_list.append('-72.9470')
+        add_list.append('West Haven, CT')
+    
+#break up large lines
+    
+for (columnName, columnData) in response_df.iteritems():
+    for row in range(len(columnData)):
+        cell = str(columnData.iloc[row])
+        count = 0
+        overall_count = 0
+        if cell == None:
+            response_df.at[row, columnName] = ' '
+        for char in cell:
+            if count >= 50 and char == ' ':
+                newstring = cell[:overall_count] + '<br>' + cell[overall_count:]
+                response_df.at[row, columnName] = newstring
+                cell = newstring
+                count = 0
+            count +=1
+            overall_count += 1
     
 response_df['Latitude'] = lat_list
 response_df['Longitude'] = long_list
+response_df['Geolocated address'] = add_list
+
+colors_dict = {'Organization':              'aqua',
+                'Business':                 'blue',
+                'Performance Venue':        'lime',
+                'Gallery/Museum':           'pink', 
+                'School/Education':         'gold',
+                'Landmark/Public Space':    'green',
+                'Event/Festival':           'red',
+                'Artist(s) Studio/Workshop':'orange',
+                'House of Worship':         'purple',
+                'Library':                  'brown'}
+
 
 for index, row in response_df.iterrows():
     
-    caption_info = row['Name'] + "<br> - Category: " + row['Category'] + "<br> - Type: " + row['Type']+"<br> - Location: " + row['Location'] + "<br> - Notes: " + row['Notes'] 
+    caption_info = f"<b>{row['Name of the place/activity you wish to add to map']}</b> <br> <b>Category:</b> {row['Category - What is it?']} <br><b>Type:</b> {row['Type']} <br><b>Location:</b> {row['Location']} <br><b>Notes:</b> {row['Notes']}" 
     
-    folium.CircleMarker(
+    if row['Type'] in colors_dict.keys():
+    
+        folium.CircleMarker(
                 # Latitude, longitude for each marker
                 location=[row['Latitude'], row['Longitude']],
         
                 # Size, fill, color of the circle marker
-                radius = 10, fill=True, color='orange', #(row['FY18.Max.Population.Count']+1)/25, fill = True, color='orange',
+                
+                    radius = 10, fill=True, color=colors_dict[row['Type']], #(row['FY18.Max.Population.Count']+1)/25, fill = True, color='orange',
+        
+                # Text that goes into the popup or tooltip
+                tooltip = caption_info 
+                  
+                 ).add_to(base_map)
+
+    else:
+
+         folium.CircleMarker(
+                # Latitude, longitude for each marker
+                location=[row['Latitude'], row['Longitude']],
+        
+                # Size, fill, color of the circle marker
+                
+                    radius = 10, fill=True, color='gray', #(row['FY18.Max.Population.Count']+1)/25, fill = True, color='orange',
         
                 # Text that goes into the popup or tooltip
                 tooltip = caption_info 
